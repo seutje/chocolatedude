0d1ce0bdcc2f8200c60e6993632748fd08e15ccd
Steve De Jonghe <seutje@gmail.com>
2025-06-21 16:01:47 +0200

    Increase image command headers timeout

--- commands/image.js
+++ commands/image.js
@@ -15,7 +15,8 @@ module.exports = async function (message) {
         const response = await fetch(apiUrl, {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({ prompt })
+            body: JSON.stringify({ prompt }),
+            headersTimeout: 10 * 60 * 1000 // wait up to 10 minutes for headers
         });
 
         if (!response.ok) {
